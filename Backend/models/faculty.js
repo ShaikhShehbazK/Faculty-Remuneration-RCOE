1a7a67497b1f10d811b91e4c2b7f3fa09e087107
ShaikhShehbaz <shahbazshaikh485@gmail.com>
2025-07-28 22:52:37 +0530

    backend/Authentication-V2

--- Backend/models/faculty.js
+++ Backend/models/faculty.js
@@ -6,9 +6,21 @@ const facultySchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
+    phone: { type: String, required: true },
     department: { type: String, required: true },
     role: { type: String, default: "faculty" },
     baseSalary: { type: Number, required: true },
+    travelAllowance: { type: Number, required: true },
+    designation: {
+      type: String,
+      enum: [
+        "Assistant Professor",
+        "Associate Professor",
+        "Professor",
+        "External Examiner",
+      ],
+      required: true,
+    },
 
     assignedSubjects: [
       {
