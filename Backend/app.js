04d212389b8c819b82a04333d5902f50bbae2588
Bedrekar Muzammil <muzammilaslambedrekar@gmail.com>
2025-09-04 08:58:59 +0530

    Minor Changes

--- Backend/app.js
+++ Backend/app.js
@@ -30,7 +30,7 @@ const app = express();
 // );
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5174",
     credentials: true, // if using cookies / sessions
   })
 );
